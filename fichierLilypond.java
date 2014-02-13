43cd4b1dfd065ee17a422961e97092ddd5feaef5
hinata1894 <irual1994@hotmail.fr>
2014-02-13 19:11:09 +0100

    Update fichierLilypond.java

--- fichierLilypond.java
+++ fichierLilypond.java
@@ -4,27 +4,60 @@ import java.io.IOException;
 import java.io.Serializable;
 import java.util.LinkedList;
 
+@SuppressWarnings("serial")
 public class fichierLilypond implements Serializable{
 	
-	public static void sauvegarder(LinkedList<Note> note){//Enregistrer au format lilypond
-		try{
-			String s = "\\include \"italiano.ly\" \\score{{";
-			//problème sur les octaves quand il y a plusieurs note
-			for(int i = 0; i < note.size(); i++){
-				if ((note.get(i)).getNumeroOctave() == 1){//1ere octave
-					s += (note.get(i)).getNomNote() + " ";
-				}
-				else if((note.get(i)).getNumeroOctave() == 2){//2eme octave
-					s += (note.get(i)).getNomNote() + "' ";
-				}
-				else if((note.get(i)).getNumeroOctave() == 3){//3eme octave
-					s += (note.get(i)).getNomNote() + "'' ";
-				}
-				else{//4eme octave
-					s += (note.get(i)).getNomNote() + "''' ";
-				}
+	public String convertionDureeLilypond(Note n){
+		int duree = n.getDuree();
+		if(duree == 1)
+			return "4";
+		else if (duree == 2)
+			return "2";
+		else if (duree == 3)
+			return "2.";
+		else if (duree == 4)
+			return "1";
+		return null;
+	}
+	
+	public String conversionNoteLilypond(LinkedList<Note> note){
+		String s = "";
+		Note noteActuel;
+		
+		for(int i = 0; i < note.size(); i++){
+			noteActuel = note.get(i);
+			
+			if(noteActuel.getNomNote().equals("silence")){
+				s += "r" + convertionDureeLilypond(noteActuel);
 			}
-			s += " }} ";
+			
+			if (noteActuel.getNumeroOctave() == 1){//1ere octave
+				s += noteActuel.getNomNote() + convertionDureeLilypond(noteActuel) + " ";
+			}
+			else if(noteActuel.getNumeroOctave() == 2){//2eme octave
+				s += noteActuel.getNomNote() + "'" + convertionDureeLilypond(noteActuel) + " ";
+			}
+			else if(noteActuel.getNumeroOctave() == 3){//3eme octave
+				s += noteActuel.getNomNote() + "''"  + convertionDureeLilypond(noteActuel) + " ";
+			}
+			else{//4eme octave
+				s += noteActuel.getNomNote() + "'''"  + convertionDureeLilypond(noteActuel) + " ";
+			}
+		}
+		return s;
+	}
+	
+	public void sauvegarderEnLilypond(LinkedList<Note> soprano, LinkedList<Note> alto, LinkedList<Note> tenor, LinkedList<Note> basse){//Enregistrer au format lilypond
+		try{
+			String s = "\\include \"italiano.ly\" \n\\score{\n\\new ChoirStaff <<\n\t\\new Staff \\with { instrumentName = \"Soprano\" }{\\new voice{ ";
+			s += conversionNoteLilypond(soprano)+"}}\n";
+			s += "\t\\new Staff \\with { instrumentName = \"Alto\" }{\\new voice{";
+			s += conversionNoteLilypond(alto)+"}}\n";
+			s += "\t\\new Staff \\with { instrumentName = \"Tenor\" }{\\new voice{";
+			s += conversionNoteLilypond(tenor)+"}}\n";
+			s += "\t\\new Staff \\with { instrumentName = \"Basse\" }{\\new voice{\\clef bass ";
+			s += conversionNoteLilypond(basse)+"}}\n";
+			s += ">>\n} ";
 			
 			FileWriter sortie = new FileWriter("test.ly");
 			sortie.write(s);
@@ -35,3 +68,5 @@ public class fichierLilypond implements Serializable{
 		}
 
 	}
+	
+}
