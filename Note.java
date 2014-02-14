d1449b89346e3ed045d904f75639b89c55e6c792
hinata1894 <irual1994@hotmail.fr>
2014-02-14 09:43:51 +0100

    Update Note.java
    Ajout d'un attribut pour le temps

--- Note.java
+++ Note.java
@@ -3,6 +3,8 @@ package projet;
 public class Note {
 	private String nomNote;
 	private int numeroOctave;
+	private int duree;
+	
 	public Note(String nomNote) throws NoteException{
 		if((! nomNote.equals("do"))&&(! nomNote.equals("re"))&&(! nomNote.equals("mi"))&&(! nomNote.equals("fa"))&&(! nomNote.equals("sol"))&&(! nomNote.equals("la"))&&(! nomNote.equals("si"))){
 			throw new NoteException();
@@ -11,6 +13,7 @@ public class Note {
 			this.nomNote = nomNote;
 		}
 	}
+	
 	public Note(String nomNote, int num) throws NoteException, NumberException{
 		if((! nomNote.equals("do"))&&(! nomNote.equals("re"))&&(! nomNote.equals("mi"))&&(! nomNote.equals("fa"))&&(! nomNote.equals("sol"))&&(! nomNote.equals("la"))
 				&&(! nomNote.equals("si"))){
@@ -23,6 +26,23 @@ public class Note {
 		} else {
 			numeroOctave = num;
 		}
+	}
+	
+	public Note(String nomNote, int num, int temps) throws NoteException, NumberException{
+		if((! nomNote.equals("do"))&&(! nomNote.equals("re"))&&(! nomNote.equals("mi"))&&(! nomNote.equals("fa"))&&(! nomNote.equals("sol"))&&(! nomNote.equals("la"))
+				&&(! nomNote.equals("si") && (!nomNote.equals("silence")))){
+			throw new NoteException();
+		}else {
+			this.nomNote = nomNote;
+		}
+		
+		if((num<1)||(num>4)){
+			throw new NumberException();
+		} else {
+			numeroOctave = num;
+		}
+		
+		duree = temps;
 	}
 	
 	public String getNomNote() {
@@ -40,6 +60,14 @@ public class Note {
 	public void setNumeroOctave(int numeroOctave) {
 		this.numeroOctave = numeroOctave;
 	}
+	
+	public int getDuree() {
+		return duree;
+	}
+	public void setDuree(int duree) {
+		this.duree = duree;
+	}
+	
 	public String toString(){
 		return nomNote+numeroOctave;
 	}
