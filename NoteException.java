b0e1282dc5d089a347ae0677320994c3fbd1fcd2
Lyviane <adrielle.yougang@etu.univ-orleans.fr>
2014-02-17 01:09:11 +0000

    Lilypond
    ajout du fichier lilypond

--- NoteException.java
+++ NoteException.java
@@ -11,5 +11,19 @@ public class NoteException extends Exception {
 		// TODO Auto-generated constructor stub
 	}
 
+	public NoteException(Throwable arg0) {
+		super(arg0);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoteException(String arg0, Throwable arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	public NoteException(String arg0, Throwable arg1, boolean arg2, boolean arg3) {
+		super(arg0, arg1, arg2, arg3);
+		// TODO Auto-generated constructor stub
+	}
 
 }
